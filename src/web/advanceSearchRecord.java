ff9bf507e71f00a8c725d61bc51041d5eddb7586
sayantan1821 <ksayantan1823@gmail.com>
2022-03-17 22:06:56 +0530

    update advance search api

--- src/web/advanceSearchRecord.java
+++ src/web/advanceSearchRecord.java
@@ -3,6 +3,7 @@ package web;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -26,76 +27,65 @@ public class advanceSearchRecord extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		String doc_id = (req.getParameter("doc_id").length() > 0) ? req.getParameter("doc_id") : null;
-		String cust_number = (req.getParameter("cust_number").length() > 0) ? req.getParameter("cust_number") : null;
-		String buisness_year = (req.getParameter("buisness_year").length() > 0) ? req.getParameter("buisness_year") : null;
-		String invoice_id = req.getParameter("invoice_id").length() > 0 ? req.getParameter("invoice_id") : null;
+		String doc_id = null, invoice_id = null, cust_number = null, buisness_year = null;
+		String query = null;
 		int pageNo = 0, recordsPerPage = 10;
+		PrintWriter out = res.getWriter();
+		Connection con = null;
+		PreparedStatement pst = null;
+
+		if (req.getParameter("doc_id") != null && req.getParameter("doc_id").length() > 0)
+			doc_id = req.getParameter("doc_id");
+		if (req.getParameter("invoice_id") != null && req.getParameter("invoice_id").length() > 0)
+			invoice_id = req.getParameter("invoice_id");
+		if (req.getParameter("cust_number") != null && req.getParameter("cust_number").length() > 0)
+			cust_number = req.getParameter("cust_number");
+		if (req.getParameter("buisness_year") != null && req.getParameter("buisness_year").length() > 0)
+			buisness_year = req.getParameter("buisness_year");
+
 		if (req.getParameter("pageNo") != null)
 			pageNo = Integer.parseInt(req.getParameter("pageNo"));
 		if (req.getParameter("recordsPerPage") != null)
 			recordsPerPage = Integer.parseInt(req.getParameter("recordsPerPage"));
 		int startIndex = pageNo * recordsPerPage;
 
-		String query = null;
-		String columns = "winter_internship.sl_no, winter_internship.business_code,"
-				+ " business.business_name, winter_internship.cust_number, customer.name_customer,"
-				+ " winter_internship.clear_date, winter_internship.buisness_year,"
-				+ " winter_internship.doc_id, winter_internship.posting_date,"
-				+ " winter_internship.document_create_date, winter_internship.document_create_date1,"
-				+ " winter_internship.due_in_date, winter_internship.invoice_currency,"
-				+ " winter_internship.document_type, winter_internship.posting_id,"
-				+ " winter_internship.area_business, winter_internship.total_open_amount,"
-				+ " winter_internship.baseline_create_date, winter_internship.cust_payment_terms,"
-				+ " winter_internship.invoice_id, winter_internship.isOpen,"
-				+ " winter_internship.aging_Bucket, winter_internship.is_deleted," + " winter_internship.predicted";
-
-		PrintWriter out = res.getWriter();
 		try {
-			Connection con = connectDB.getConnection();
-			Statement st = con.createStatement();
+			con = connectDB.getConnection();
+			query = "SELECT * FROM winter_internship WHERE ( " + doc_id + " IS NULL OR doc_id LIKE '" + doc_id
+					+ "%') AND ( " + cust_number + " IS NULL OR cust_number LIKE '" + cust_number + "%') AND ( "
+					+ invoice_id + " IS NULL OR invoice_id LIKE '" + invoice_id + "%') AND ( " + buisness_year
+					+ " IS NULL OR buisness_year LIKE '" + buisness_year + "%') AND is_deleted = 0 LIMIT " 
+					+ startIndex + "," + recordsPerPage;
+			pst = con.prepareStatement(query);
 
-			st = con.createStatement();
-			query = "SELECT " + columns + " FROM " + dbCredentials.getTableName()
-					+ " LEFT JOIN `business` ON `winter_internship`.`business_code` ="
-					+ " `business`.`business_code` LEFT JOIN `customer` ON"
-					+ " `winter_internship`.`cust_number` = `customer`.`cust_number` WHERE (" + doc_id
-					+ " IS NULL OR doc_id LIKE '" + doc_id + "%') AND (" + cust_number
-					+ " IS NULL OR winter_internship.cust_number LIKE '" + cust_number + "%') AND (" + invoice_id
-					+ " IS NULL OR winter_internship.invoice_id LIKE '" + invoice_id + "%') AND (" + buisness_year
-					+ " IS NULL OR buisness_year LIKE '" + buisness_year + "%') AND winter_internship.is_deleted = 0 LIMIT "
-					+ startIndex + ", " + recordsPerPage;
-
-			ResultSet rs2 = st.executeQuery(query);
+			ResultSet rs = pst.executeQuery(query);
 
 			ArrayList<MainPojo> pojoArray = new ArrayList<MainPojo>();
-			while (rs2.next()) {
+			while (rs.next()) {
 				MainPojo pojo = new MainPojo();
+				pojo.setSl_no(Integer.parseInt(rs.getString("sl_no")));
+				pojo.setBusiness_code(rs.getString("business_code"));
+				pojo.setCust_number(rs.getString("cust_number"));
+				pojo.setClear_date(rs.getString("clear_date"));
+				pojo.setBusiness_year(rs.getString("buisness_year"));
+				pojo.setDoc_id(rs.getString("doc_id"));
+				pojo.setPosting_date(rs.getString("posting_date"));
+				pojo.setDocument_create_date(rs.getString("document_create_date"));
+				pojo.setDocument_create_date1(rs.getString("document_create_date1"));
+				pojo.setDue_in_date(rs.getString("due_in_date"));
+				pojo.setInvoice_currency(rs.getString("invoice_currency"));
+				pojo.setDocument_type(rs.getString("document_type"));
+				pojo.setPosting_id(Integer.parseInt(rs.getString("posting_id")));
+				pojo.setArea_business(rs.getString("area_business"));
+				pojo.setTotal_open_amount(rs.getDouble("total_open_amount"));
+				pojo.setBaseline_create_date(rs.getString("baseline_create_date"));
+				pojo.setCust_payment_terms(rs.getString("cust_payment_terms"));
+				pojo.setInvoice_id(rs.getInt("invoice_id"));
+				pojo.setIsOpen(rs.getInt("isOpen"));
+				pojo.setAging_Bucket(rs.getString("aging_Bucket"));
+				pojo.setIs_deleted(rs.getString("is_deleted"));
+				pojo.setPredicted(rs.getString("predicted"));
 
-				pojo.setSl_no(Integer.parseInt(rs2.getString("sl_no")));
-				pojo.setBusiness_code(rs2.getString("business_code"));
-				pojo.setBusiness_name(rs2.getString("business_name"));
-				pojo.setCust_number(rs2.getString("cust_number"));
-				pojo.setName_customer(rs2.getString("name_customer"));
-				pojo.setClear_date(rs2.getString("clear_date"));
-				pojo.setBusiness_year(rs2.getString("buisness_year"));
-				pojo.setDoc_id(rs2.getString("doc_id"));
-				pojo.setPosting_date(rs2.getString("posting_date"));
-				pojo.setDocument_create_date(rs2.getString("document_create_date"));
-				pojo.setDocument_create_date1(rs2.getString("document_create_date1"));
-				pojo.setDue_in_date(rs2.getString("due_in_date"));
-				pojo.setInvoice_currency(rs2.getString("invoice_currency"));
-				pojo.setDocument_type(rs2.getString("document_type"));
-				pojo.setPosting_id(Integer.parseInt(rs2.getString("posting_id")));
-				pojo.setArea_business(rs2.getString("area_business"));
-				pojo.setTotal_open_amount(rs2.getDouble("total_open_amount"));
-				pojo.setBaseline_create_date(rs2.getString("baseline_create_date"));
-				pojo.setCust_payment_terms(rs2.getString("cust_payment_terms"));
-				pojo.setInvoice_id(rs2.getInt("invoice_id"));
-				pojo.setIsOpen(rs2.getInt("isOpen"));
-				pojo.setAging_Bucket(rs2.getString("aging_Bucket"));
-				pojo.setIs_deleted(rs2.getString("is_deleted"));
-				pojo.setPredicted(rs2.getString("predicted"));
 				pojoArray.add(pojo);
 			}
 			GsonBuilder gb = new GsonBuilder();
@@ -106,10 +96,10 @@ public class advanceSearchRecord extends HttpServlet {
 			res.setCharacterEncoding("UTF-8");
 
 			out.println(jsonData);
-			rs2.close();
+			rs.close();
 
 			con.close();
-			st.close();
+			pst.close();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
