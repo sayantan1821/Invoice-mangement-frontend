311ed28aebbb063bf48a0e6a267de90d8318f358
sayantan1821 <ksayantan1823@gmail.com>
2022-03-26 22:10:53 +0530

    add analytics api

---
+++ src/web/AnalyticsPojo.java
@@ -0,0 +1,29 @@
+package web;
+
+public class AnalyticsPojo {
+	private String business_code;
+	private int no_of_cust;
+	private double sum_of_open_amount;
+	
+	
+	public String getBusiness_code() {
+		return business_code;
+	}
+	public void setBusiness_code(String business_code) {
+		this.business_code = business_code;
+	}
+	public int getNo_of_cust() {
+		return no_of_cust;
+	}
+	public void setNo_of_cust(int no_of_cust) {
+		this.no_of_cust = no_of_cust;
+	}
+	public double getSum_of_open_amount() {
+		return sum_of_open_amount;
+	}
+	public void setSum_of_open_amount(double sum_of_open_amount) {
+		this.sum_of_open_amount = sum_of_open_amount;
+	}
+	
+	
+}
